38dfa6ecf86e096a04b309521643fb54a372a51b
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-01 19:07:01 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  arr = []
+end
+
+def array_with_two_elements
+  return arr = [1, 2]
+end
+
+def first_element(arr)
+  return arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  return arr.first
+end
+
+def last_element_with_array_methods(arr)
+  arr.last
+end
+
+def length_of_array(arr)
+  arr.length
+end
