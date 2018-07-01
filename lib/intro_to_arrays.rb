e83b9ec965f3919b69aac1d161f9ec30b339d4e4
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-01 19:04:28 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,15 @@ end
 def last_element(arr)
   arr[-1]
 end
+
+def first_element_with_array_method(arr)
+  return arr.first
+end
+
+def last_element_with_array_method(arr)
+  arr.last
+end
+
+def length_of_array(arr)
+  arr.
+end
